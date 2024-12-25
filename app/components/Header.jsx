54258d59cea67a397c4b2f9b7b1333837e2b0d5a
Rukiye Canlı <canli.rukiye@std.izu.edu.tr>
2024-12-25 19:23:14 +0300

    logo added

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="header">
-      <div><img src="./logo.png" alt="logo" /></div>
+      <div style={{display: "flex", alignItems: "center"}}>
+        <img src="./logo.png" alt="logo" />
+        <img src="./logoYazi.png" alt="logoYazi" width={"180px"} height={"25px"}/>
+      </div>
       <div>
         <ul className="nav-list">
             <li className="home"><Link href="/">Anasayfa</Link></li>
